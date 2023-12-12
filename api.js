de7342e73a856bb42f3dc1de81be5bd9530439e9
seonyo <jsy6535@naver.com>
2023-12-12 11:48:06 +0900

    주석 제거

--- api.js
+++ api.js
@@ -9,13 +9,13 @@ const port = 3001
 app.use(express.json());
 app.use(cors());
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//     port: 3306
-// })
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 3306
+})
 
 const pool = mysql.createPool({
     host: 'localhost',
